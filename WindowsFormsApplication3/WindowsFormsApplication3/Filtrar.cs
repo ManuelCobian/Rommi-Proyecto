dd106a37a1ca45ee7e51cb1d7b43d66d3a9bfb34
Manuel Cobian <lcobian_@ucol.mx>
2016-11-28 09:16:50 -0600

    Clase filtrado

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Filtrar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication3
+{
+    class Filtrar
+    {
+        String value = "This is a short string.";
+        Char delimiter = 's';
+
+        public void filtrar()
+        {
+            String[] substrings = value.Split(delimiter);
+            foreach (var substring in substrings)
+                Console.WriteLine(substring);
+        }
+       
+    }
+}
